1c482a2ade51a3de85a5a6a99ce0d29822770d4c
Hakan <hakan@yaman.uk>
2023-10-12 12:26:56 +0300

    most sold

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ import "@fortawesome/fontawesome-free/js/all.js"
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+// add to cart in most sold section
+document.addEventListener("DOMContentLoaded", function() {
+    // Select all buttons within the specific div
+    let buttons = document.querySelectorAll(".carousel-inner button");
+  
+    // Loop through each button and add the event listener
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function() {
+        alert("تم اضافة المنتج إلى عربة التسوق");
+      });
+    });
+  });
